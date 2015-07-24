98c0851c50f4dd04a2cbc643b6c20489b2922a2a
Ning Leng <lengning1@gmail.com>
2015-07-24 15:26:44 -0700

    boost

--- R/MedianNorm.R
+++ R/MedianNorm.R
@@ -1,5 +1,17 @@
-MedianNorm=function(Data){
+MedianNorm <- function(Data, alternative=FALSE){
 	if(ncol(Data)==1)stop("Only 1 sample!")
-  geomeans <- exp(rowMeans(log(Data)))
-	apply(Data, 2, function(cnts) median((cnts/geomeans)[geomeans >  0]))
+  if(!alternative){
+		geomeans <- exp(rowMeans(log(Data)))
+		out <- apply(Data, 2, function(cnts) median((cnts/geomeans)[geomeans >  0]))}
+	if(alternative){
+		DataMatO <- Data
+		N <- ncol(DataMatO)
+		DataList0 <- sapply(1:N,function(i)DataMatO[,i]/DataMatO,simplify=F)
+		DataEachMed0 <- sapply(1:N,function(i)apply(DataList0[[i]],2,function(j)median(j[which(j>0
+		& j<Inf)])))
+		DataColgeo <- sapply(1:N,function(i)exp(mean(log(DataEachMed0[-i,i]))))
+		out <- DataColgeo
+		}
+
+	out
 }
